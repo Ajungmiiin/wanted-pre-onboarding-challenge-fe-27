aba0ceb8dc18a7219f54ae270463e95e8cb29842
Ajunmiiin <wjdals3249@naver.com>
2024-10-30 21:41:36 +0900

    design : 초기화면 UI

--- src/routes/pages/Home.tsx
+++ src/routes/pages/Home.tsx
@@ -1,5 +1,22 @@
+import { Link } from 'react-router-dom';
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <section className="flex flex-col gap-4">
+      <Link
+        to="/auth?mode=login"
+        className="bg-blue-500 text-white w-[300px] text-center py-4 text-2xl rounded-xl hover:bg-opacity-90 transition-colors"
+      >
+        로그인
+      </Link>
+      <Link
+        to="/auth?mode=sign-up"
+        className="bg-blue-500 text-white w-[300px] text-center py-4 text-2xl rounded-xl hover:bg-opacity-90 transition-colors"
+      >
+        회원가입
+      </Link>
+    </section>
+  );
 };
 
 export default Home;

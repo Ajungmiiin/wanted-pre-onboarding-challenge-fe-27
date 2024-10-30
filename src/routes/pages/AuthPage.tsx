1b3eca4332b79c96dfdc707559bd94c05dbf438b
Ajunmiiin <wjdals3249@naver.com>
2024-10-30 21:42:48 +0900

    feat : Auth의 모드가 유효하지 않을 때 초기화면으로 Redirect

--- src/routes/pages/AuthPage.tsx
+++ src/routes/pages/AuthPage.tsx
@@ -1,5 +1,23 @@
+import { useEffect } from 'react';
+import { useNavigate, useSearchParams } from 'react-router-dom';
+import AuthForm from '../../feature/auth/AuthForm';
+
 const AuthPage = () => {
-  return <div>AuthPage</div>;
+  const [searchParams] = useSearchParams();
+  const navigate = useNavigate();
+  const authMode = searchParams.get('mode');
+
+  useEffect(() => {
+    if (authMode !== 'login' && authMode !== 'sign-up') {
+      navigate('/');
+    }
+  }, []);
+
+  return (
+    <section>
+      <AuthForm mode={authMode as string} />
+    </section>
+  );
 };
 
 export default AuthPage;

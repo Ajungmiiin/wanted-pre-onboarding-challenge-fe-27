6d169b0ffb9db833385017f4adb0f2372ff5b08d
Ajunmiiin <wjdals3249@naver.com>
2024-11-01 18:03:59 +0900

    Feat : tanstack-query Provier 추가

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,6 +4,9 @@ import DefaultLayout from './layouts/DefaultLayout';
 
 import AuthPage from './pages/AuthPage';
 import Home from './pages/Home';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 const router = createBrowserRouter([
   {
@@ -23,7 +26,11 @@ const router = createBrowserRouter([
 ]);
 
 const Router = () => {
-  return <RouterProvider router={router} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  );
 };
 
 export default Router;

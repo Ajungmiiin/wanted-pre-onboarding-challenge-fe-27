3f7332f9d1f22af2929fd2273006eb45513d48fb
Ajunmiiin <wjdals3249@naver.com>
2024-11-03 16:38:56 +0900

    Feat : 로그인이 되어있는 경우 사용자를 루트페이지로 redirect

--- src/routes/pages/AuthPage.tsx
+++ src/routes/pages/AuthPage.tsx
@@ -1,21 +1,27 @@
 import { useEffect } from 'react';
 import { useNavigate, useSearchParams } from 'react-router-dom';
 import AuthForm from '../../feature/auth/AuthForm';
+import { authMode } from '../../types/AuthMode';
+import { getToken } from '../../utils/getToken';
 
 const AuthPage = () => {
   const [searchParams] = useSearchParams();
   const navigate = useNavigate();
   const authMode = searchParams.get('mode');
-
+  const token = getToken();
   useEffect(() => {
-    if (authMode !== 'login' && authMode !== 'sign-up') {
+    if (token) {
       navigate('/');
+    }
+
+    if (authMode !== 'login' && authMode !== 'sign-up') {
+      navigate('/auth?mode=login');
     }
   }, []);
 
   return (
     <section>
-      <AuthForm mode={authMode as string} />
+      <AuthForm mode={authMode as authMode} />
     </section>
   );
 };

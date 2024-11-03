9714ca51f0819c985c69ac5cff92e9c724c6e5d9
Ajunmiiin <wjdals3249@naver.com>
2024-11-03 16:37:44 +0900

    Feat : 로그인하지 않았을 경우 auth페이지로 redirect & 투두리스트를 불러오는 로직 추가

--- src/routes/pages/Home.tsx
+++ src/routes/pages/Home.tsx
@@ -1,21 +1,52 @@
-import { Link } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Link, Outlet, useNavigate } from 'react-router-dom';
+
+import useGetTodos from '../../hooks/useGetTodos';
+
+import { getToken } from '../../utils/getToken';
+import TodoList from '../../feature/todo/TodoList';
+import { Todo } from '../../types/Todo';
+import LogoutButton from '../../feature/button/LogoutButton';
 
 const Home = () => {
+  const token = getToken();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!token) {
+      navigate('/auth?mode=login');
+    }
+  }, []);
+
+  const { data, isPending } = useGetTodos(token as string);
+
   return (
-    <section className="flex flex-col gap-4">
-      <Link
-        to="/auth?mode=login"
-        className="bg-blue-500 text-white w-[300px] text-center py-4 text-2xl rounded-xl hover:bg-opacity-90 transition-colors"
-      >
-        로그인
-      </Link>
-      <Link
-        to="/auth?mode=sign-up"
-        className="bg-blue-500 text-white w-[300px] text-center py-4 text-2xl rounded-xl hover:bg-opacity-90 transition-colors"
-      >
-        회원가입
-      </Link>
-    </section>
+    <>
+      <section className="min-w-[1024px] text-center p-4">
+        <div className="p-4 border-b mb-4 relative">
+          <h3 className="text-2xl text-left">ToDoS</h3>
+          <div className="right-4 top-[50%] translate-y-[-50%] absolute inline-flex gap-4">
+            <LogoutButton />
+            <Link
+              to={'/add'}
+              className=" px-4 py-2  text-white bg-blue-500 text-lg rounded-md hover:bg-blue-600 transition-colors"
+            >
+              추가하기
+            </Link>
+          </div>
+        </div>
+
+        <div className="flex min-h-[500px] gap-4">
+          {!isPending && data!.length === 0 && (
+            <p className="w-full">할 일이 없습니다.</p>
+          )}
+          {!isPending && data!.length > 0 && (
+            <TodoList todoList={data as Todo[]} />
+          )}
+          {<Outlet />}
+        </div>
+      </section>
+    </>
   );
 };
 
